8a5cf3b1cf0fa000ef76078577511e3c5f5e68e2
Shourob Datta <sourovsuet@gmail.com>
2025-09-22 12:00:37 +0200

    feature: added project structure.

---
+++ SwiftUIMVVMClean/SwiftUIMVVMCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMCleanApp.swift
+//  SwiftUIMVVMClean
+//
+//  Created by Guest Datta on 22.09.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIMVVMCleanTests/SwiftUIMVVMCleanTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMCleanTests.swift
+//  SwiftUIMVVMCleanTests
+//
+//  Created by Guest Datta on 22.09.25.
+//
+
+import Testing
+@testable import SwiftUIMVVMClean
+
+struct SwiftUIMVVMCleanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
